c98373fb431d0fc88686b316ce3d99ca060f1b27
Ayush Padmawar <156106810+ayush0521@users.noreply.github.com>
2026-04-05 08:47:42 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI
 from backend.routes import router
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # for hackathon
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
