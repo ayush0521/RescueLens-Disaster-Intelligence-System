bf60bda0de46e38ef44dce204982bc8fdda97aea
Ayush Padmawar <156106810+ayush0521@users.noreply.github.com>
2026-04-05 08:53:52 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,18 @@
 from fastapi import FastAPI
-from backend.routes import router
-
-app = FastAPI()
 from fastapi.middleware.cors import CORSMiddleware
 
+from routes import router   # IMPORTANT: no "backend."
+
+app = FastAPI()
+
+# CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # for hackathon
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-print("CORS LOADED SUCCESSFULLY")
 
-@app.get("/")
-def home():
-    return {"message": "API is running"}
-
+# INCLUDE ROUTER
 app.include_router(router)
